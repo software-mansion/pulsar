d31354f73763820e9dd55e963822aab5e1d952bd
AnCichocka <annacichocka.poczta@gmail.com>
2025-11-24 09:33:42 +0100

    format presets

--- Android/PulsarApp/app/src/main/java/com/swmansion/pulsarapp/Presets.kt
+++ Android/PulsarApp/app/src/main/java/com/swmansion/pulsarapp/Presets.kt
@@ -8,10 +8,11 @@ import kotlin.collections.arrayListOf
 val SUCCESS_PRESET =
   Preset(
     name = "Success",
-    barsList = arrayListOf(
-      Bar(0, 100, 0.809f, 0.616f),
-      Bar(200, 300, 0.809f, 0.619f),
-      Bar(550, 650, 1f, 1f)),
+    barsList =
+      arrayListOf(
+        Bar(0, 100, 0.809f, 0.616f),
+        Bar(200, 300, 0.809f, 0.619f),
+        Bar(550, 650, 1f, 1f)),
   )
 val FAIL_PRESET =
   Preset(
@@ -20,7 +21,8 @@ val FAIL_PRESET =
       arrayListOf(
         Bar(0, 100, 0.809f, 0.616f),
         Bar(200, 300, 0.809f, 0.619f),
-        Bar(550, 650, 0.591f, 0.309f)),
+        Bar(550, 650, 0.591f, 0.309f),
+      ),
   )
 val ENVELOPE_TEST_PRESET =
   Preset(
@@ -37,43 +39,46 @@ val ENVELOPE_TEST_PRESET =
 val FALLING_BRICKS =
   Preset(
     name = "Falling Bricks",
-    barsList = arrayListOf(
-      Bar(0, 100, 1f, 1f),
-      Bar(200, 300, 0.675f, 0.675f),
-      Bar(400, 500, 0.406f, 0.2f),
-      Bar(600, 700, 0.659f, 0.659f),
-      Bar(800, 900, 0.941f, 0.941f)
-    )
+    barsList =
+      arrayListOf(
+        Bar(0, 100, 1f, 1f),
+        Bar(200, 300, 0.675f, 0.675f),
+        Bar(400, 500, 0.406f, 0.2f),
+        Bar(600, 700, 0.659f, 0.659f),
+        Bar(800, 900, 0.941f, 0.941f),
+      ),
   )
 val EARTHQUAKE_PRESET =
   Preset(
     name = "Earthquake",
-    pointsList = arrayListOf(
-      EnvelopePoint(0f, 1f, 0),
-      EnvelopePoint(0.8f, 1f, 400),
-      EnvelopePoint(0f, 1f, 400),
-      EnvelopePoint(0f, 1f, 500),
-      EnvelopePoint(0.8f, 1f, 700),
-      EnvelopePoint(0f, 1f, 700),
-      )
+    pointsList =
+      arrayListOf(
+        EnvelopePoint(0f, 1f, 0),
+        EnvelopePoint(0.8f, 1f, 400),
+        EnvelopePoint(0f, 1f, 400),
+        EnvelopePoint(0f, 1f, 500),
+        EnvelopePoint(0.8f, 1f, 700),
+        EnvelopePoint(0f, 1f, 700),
+      ),
   )
 val RANDOM_PRESET =
   Preset(
     name = "Random Preset",
-    pointsList = arrayListOf(
-      EnvelopePoint(0f, 1f, 0),
-      EnvelopePoint(0.8f, 1f, 400),
-      EnvelopePoint(0f, 1f, 400),
-      EnvelopePoint(0f, 1f, 500),
-      EnvelopePoint(0.8f, 1f, 700),
-      EnvelopePoint(0f, 1f, 700),
-      EnvelopePoint(0f, 1f, 900),
-      EnvelopePoint(1f, 0.9f, 900),
-      EnvelopePoint(1f, 0.9f, 1000),
-      EnvelopePoint(0f, 0.9f, 1000),
-      EnvelopePoint(0f, 0.9f, 1100),
-      EnvelopePoint(1f, 0.9f, 1100),
-      EnvelopePoint(1f, 0.9f, 1200),
-      EnvelopePoint(0f, 0.9f, 1200),
-      )
+    pointsList =
+      arrayListOf(
+        EnvelopePoint(0f, 1f, 0),
+        EnvelopePoint(0.8f, 1f, 400),
+        EnvelopePoint(0f, 1f, 400),
+        EnvelopePoint(0f, 1f, 500),
+        EnvelopePoint(0.8f, 1f, 700),
+        EnvelopePoint(0f, 1f, 700),
+        EnvelopePoint(0f, 1f, 900),
+        EnvelopePoint(1f, 0.9f, 900),
+        EnvelopePoint(1f, 0.9f, 1000),
+        EnvelopePoint(0f, 0.9f, 1000),
+        EnvelopePoint(0f, 0.9f, 1100),
+        EnvelopePoint(1f, 0.9f, 1100),
+        EnvelopePoint(1f, 0.9f, 1200),
+        EnvelopePoint(0f, 0.9f, 1200),
+      ),
   )
