27c06230ab4760489799e47b59b4ed88747ee568
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-01-27 23:33:58 +0100

    Remove unused imports

--- react-native/react-native-pulsar/android/src/main/java/com/swmansion/pulsar/types/RealtimeComposer.kt
+++ react-native/react-native-pulsar/android/src/main/java/com/swmansion/pulsar/types/RealtimeComposer.kt
@@ -1,5 +1,10 @@
 package com.swmansion.pulsar.types
 
+/**
+ * @param ENVELOPE - TODO
+ * @param PRIMITIVE_TICK - TODO
+ * @param PRIMITIVE_COMPLEX - TODO
+ */
 enum class RealtimeComposerStrategy {
     ENVELOPE,
     PRIMITIVE_TICK,
