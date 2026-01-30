5562076da868bbc565a744cfcab52c780f4cccc9
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-01-30 13:59:55 +0100

    Remove docs backup

--- docs/src/content/docs_backup/playground/components/config.ts
+++
@@ -1,6 +0,0 @@
-// export const API_SERVER_URL = 'https://haptics-server.onrender.com';
-export const API_SERVER_URL = 'http://localhost:8080';
-
-// export const SOCKET_SERVER_URL = 'wss://haptics-server.onrender.com;
-export const SOCKET_SERVER_URL = 'ws://localhost:8080';
-
