056a3b3a5e52900014c3b7efe99227fa08e7f7d0
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-02-25 13:02:17 +0100

    Connection config

--- PulsarApp/constants/Connection.ts
+++ PulsarApp/constants/Connection.ts
@@ -1,6 +1,6 @@
 export const API_SERVER_URL = 'https://pulsar-copy.onrender.com';
 // export const API_SERVER_URL = 'http://localhost:8080';
 
-// export const SOCKET_SERVER_URL = 'wss://pulsar-copy.onrender.com';
-export const SOCKET_SERVER_URL = 'ws://localhost:8080';
+export const SOCKET_SERVER_URL = 'wss://pulsar-copy.onrender.com';
+// export const SOCKET_SERVER_URL = 'ws://localhost:8080';
 
