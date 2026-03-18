807551f49af73193fc220c7bc8321f7659a55eea
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-18 15:46:14 +0100

    Update links

--- docs/src/content/docs/components/config.ts
+++ docs/src/content/docs/components/config.ts
@@ -1,5 +1,5 @@
-export const API_SERVER_URL = 'https://pulsar-server.swmansion.com/';
+export const API_SERVER_URL = 'https://pulsar-server.swmansion.com';
 // export const API_SERVER_URL = 'http://localhost:8080';
 
-export const SOCKET_SERVER_URL = 'wss://pulsar-server.swmansion.com/';
+export const SOCKET_SERVER_URL = 'wss://pulsar-server.swmansion.com';
 // export const SOCKET_SERVER_URL = 'ws://localhost:8080';
