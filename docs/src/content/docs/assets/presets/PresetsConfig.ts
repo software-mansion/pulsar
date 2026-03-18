785c24f6adc7e063128f02266455a7578bc1542c
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-18 13:37:51 +0100

    Adjust docs to codegen script

--- docs/src/content/docs/assets/presets/PresetsConfig.ts
+++ docs/src/content/docs/assets/presets/PresetsConfig.ts
@@ -1,14 +1,13 @@
-import AimingFirePreset from './AimingFire.json';
-import AimingFireImage from './AimingFire.png';
 import type { PresetConfig } from '../../components/Preset/types';
 
+// CODEGEN_BEGIN_{imports}
+import AimingFirePreset from './AimingFire.json';
+import AimingFireImage from './AimingFire.png';
+// CODEGEN_END_{imports}
+
 export const PresetsConfig: Array<PresetConfig> = [
-  {
-    data: AimingFirePreset,
-    image: AimingFireImage,
-  },
-  {
-    data: AimingFirePreset,
-    image: AimingFireImage,
-  },
+// CODEGEN_BEGIN_{presets}
+  { data: AimingFirePreset, image: AimingFireImage },
+  { data: AimingFirePreset, image: AimingFireImage },
+// CODEGEN_END_{presets}
 ];
--- docs/src/content/docs/assets/presets/index.ts
+++
@@ -1,7 +0,0 @@
-import image from './AimingFire.png';
-import data from './AimingFire.json';
-import type { PresetConfig } from '../../components/Preset/types';
-export default {
-  image,
-  data,
-} as PresetConfig;
