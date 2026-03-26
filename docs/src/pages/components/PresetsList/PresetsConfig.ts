365e366bf7a9d18d177be5a7757817be92835296
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-26 21:55:32 +0100

    Update preset on main page

--- docs/src/pages/components/PresetsList/PresetsConfig.ts
+++ docs/src/pages/components/PresetsList/PresetsConfig.ts
@@ -1,5 +1,7 @@
 import AlarmPreset from '../../../content/docs/assets/presets/Alarm.json';
 import AlarmImage from '../../../content/docs/assets/presets/Alarm.png';
+import FadeOutPreset from '../../../content/docs/assets/presets/FadeOut.json';
+import FadeOutImage from '../../../content/docs/assets/presets/FadeOut.png';
 import type { PresetConfig } from '../../../content/docs/components/Preset/types';
 
 export const PresetsConfig: Array<PresetConfig> = [
@@ -8,7 +10,7 @@ export const PresetsConfig: Array<PresetConfig> = [
     image: AlarmImage,
   },
   {
-    data: AlarmPreset,
-    image: AlarmImage,
+    data: FadeOutPreset,
+    image: FadeOutImage,
   },
 ];
