dfbbe56d8b3e1ff53ade548c5fa5815b35e181ec
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-30 11:13:22 +0200

    Expo doctor

--- PulsarApp/app.config.js
+++ PulsarApp/app.config.js
@@ -1,9 +1,81 @@
-// app.config.js - extends app.json with dynamic config (e.g., environment variables)
-// PostHog keys are read from .env at build time and exposed via expo-constants
-const appJson = require('./app.json')
-
+// app.config.js - dynamic config with environment variables (e.g., PostHog keys from .env)
 export default {
-  ...appJson.expo,
+  name: "PulsarApp",
+  slug: "PulsarApp",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "pulsarapp",
+  userInterfaceStyle: "light",
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.swmansion.pulsar.app",
+    associatedDomains: ["applinks:pulsar.godetour.link"],
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    predictiveBackGestureEnabled: false,
+    package: "com.swmansion.pulsar.app",
+    intentFilters: [
+      {
+        action: "VIEW",
+        autoVerify: true,
+        data: [
+          {
+            scheme: "pulsarapp",
+            host: "connect",
+          },
+        ],
+        category: ["BROWSABLE", "DEFAULT"],
+      },
+      {
+        action: "VIEW",
+        autoVerify: true,
+        data: [
+          {
+            scheme: "https",
+            host: "pulsar.godetour.link",
+          },
+        ],
+        category: ["BROWSABLE", "DEFAULT"],
+      },
+    ],
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  updates: {
+    url: "https://u.expo.dev/a0480f36-5519-450f-8836-8ae64b1a9ef1",
+    checkAutomatically: "ON_LOAD",
+    fallbackToCacheTimeout: 0,
+  },
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+  plugins: [
+    "expo-updates",
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#E1F3FA",
+      },
+    ],
+    "expo-localization",
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
   owner: "piaskowyk",
   extra: {
     eas: {
