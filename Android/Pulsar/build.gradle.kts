1522a3c3d48b99b6e40505f1549390c899c8c6be
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-03-31 14:17:42 +0200

    Update maven endpoint

--- Android/Pulsar/build.gradle.kts
+++ Android/Pulsar/build.gradle.kts
@@ -97,7 +97,7 @@ afterEvaluate {
 
         repositories {
             maven {
-                url = uri("https://s01.oss.sonatype.org/service/repositories/releases/content/")
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
                 credentials {
                     username = System.getenv("MAVEN_USERNAME")
                     password = System.getenv("MAVEN_PASSWORD")
