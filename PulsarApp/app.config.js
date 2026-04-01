4fe32c2146c3ffb2fa71ce3005c75eb38bf81524
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-04-01 21:02:02 +0200

    update version of app

--- PulsarApp/app.config.js
+++ PulsarApp/app.config.js
@@ -2,7 +2,7 @@
 export default {
   name: "PulsarApp",
   slug: "PulsarApp",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "pulsarapp",
