7c8048d8020c8ca3ed08bb9bd9c87325fd3f99a7
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-04-15 10:21:29 +0200

    Update posthog integration

--- docs/config.ts
+++ docs/config.ts
@@ -1,1 +1,6 @@
 export const BASE_PATH = '/pulsar';
+
+export const POSTHOG_CONFIG = {
+  apiKey: 'phc_im7iDRXp3hg6VQ2teOcYCLVsUddGtNOn2ntKdgGk9J0',
+  apiHost: 'https://us.i.posthog.com',
+} as const;
