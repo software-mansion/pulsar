419cb4c6124e20963283b06d05e5141bba85cdef
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-04-23 13:47:30 +0200

    Update script

--- iOS/Pulsar/sync.sh
+++ iOS/Pulsar/sync.sh
@@ -1,15 +1,7 @@
 cd ../../../pulsar-ios
-# pwd
-# for file in *; do
-#     if [ "$file" != ".git" ]; then
-#         # rm -rf "$file"
-#         echo "Removing $file"
-#     fi
-# done
 cp -r ../pulsar/iOS/Pulsar/* ./
-# git add .
-# git commit -m "Sync iOS Pulsar files"
-# git push origin main
 rm sync.sh
+git add .
+git commit -m "Sync iOS Pulsar files"
+git push origin main
 cd ../pulsar/iOS/Pulsar/
-
