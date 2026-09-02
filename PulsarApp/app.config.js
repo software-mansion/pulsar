1774684c43f08bbdf1079bb126e66c7a1b4efaa2
Krzysztof Piaskowy <krzysztof.piaskowy@swmansion.com>
2026-09-02 16:55:59 +0200

    update app config (#250)

--- PulsarApp/app.config.js
+++ PulsarApp/app.config.js
@@ -8,7 +8,7 @@ const sentryPluginConfig = {
 export default {
   name: "PulsarApp",
   slug: "PulsarApp",
-  version: "1.1.0",
+  version: "1.2.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "pulsarapp",
@@ -29,7 +29,7 @@ export default {
     },
     predictiveBackGestureEnabled: false,
     package: "com.swmansion.pulsar.app",
-    versionCode: 9,
+    versionCode: 10,
     intentFilters: [
       {
         action: "VIEW",
